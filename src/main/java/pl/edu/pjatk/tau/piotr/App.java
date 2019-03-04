a03edc62db418dab16c9e37c94eb4d38cc4f968f
Vasagio <wasakmail@gmail.com>
2019-03-04 23:22:22 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pjatk/tau/piotr/App.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.tau.piotr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
